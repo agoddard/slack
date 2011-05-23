5bd38fb1f926b4d12256236cdd655f2ab67e86df
Anthony Goddard <goddardanthony@gmail.com>
2011-05-23 15:24:30 -0400

    cleaned up formatting

--- slack.rb
+++ slack.rb
@@ -4,15 +4,12 @@ require 'date'
 
 tide_html = Nokogiri::HTML(open("http://www.mobilegeographics.com:81/locations/7064.html"))
 tide_html.css('pre').to_s.each_line.each do |line|
-field = line.split(' ')
-	if field[6] == "Slack,"
-		date = Date.parse(field[0])
-		time = "#{field[1]} #{field[2]}"
-		kts = field[4]
-		event = "#{field[7]} #{field[8]}"
-		if date == Date.today+1
-      puts "#{date} #{time} #{kts} #{event}" 
-    end
-	end
+  field = line.split
+  if field[6] == "Slack,"
+    date = Date.parse field[0]
+    time = "#{field[1]} #{field[2]}"
+    kts = field[4]
+    event = "#{field[7]} #{field[8]}"
+    puts "#{date} #{time} #{kts} #{event}" if date == Date.today
+  end
 end
-
