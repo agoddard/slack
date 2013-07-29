38c642cbadce1b9151ce82c0e67b363f2f2f3ff9
Anthony Goddard <anthony@anthonygoddard.com>
2013-07-29 17:19:15 +1000

    all the tides

--- slack.rb
+++ slack.rb
@@ -5,11 +5,12 @@ require 'date'
 tide_html = Nokogiri::HTML(open("http://www.mobilegeographics.com:81/locations/7064.html"))
 tide_html.css('pre').to_s.each_line.each do |line|
   field = line.split
-  if field[6] == "Slack,"
+#p line
+  if field[5] == "knots"
     date = Date.parse field[0]
     time = "#{field[1]} #{field[2]}"
     kts = field[4]
-    event = "#{field[7]} #{field[8]}"
+    event = "#{field[6]} #{field[7]} #{field[8]}"
     puts "#{date} #{time} #{kts} #{event}" if date == Date.today
-  end
+ end
 end
